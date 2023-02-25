a2e10c555197327f7aa7845e1184994b86d2718f
Alex Brown <mail@alexbrown.info>
2023-02-25 22:20:34 +0000

    Fixed makeins.bat (my fault)

--- makeins.bat
+++ makeins.bat
@@ -7,6 +7,8 @@ deltree /Y INSTALL > NUL:
 mkdir INSTALL > NUL:
 cd INSTALL > NUL:
 mkdir APP > NUL:
+rem Copy EDisAsm
+copy ..\EDISASM.APP APP\ > NUL:
 rem 
 rem And finish back in the root directory
 cd ..
